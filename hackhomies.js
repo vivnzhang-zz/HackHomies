e70b4ce97e1f47a4fbca2d2b953e2109c9f89d5e
Vivian Zhang <vivzhang@mit.edu>
2016-01-17 19:46:21 -0500

    autofill form on createProfile

--- hackhomies.js
+++ hackhomies.js
@@ -20,19 +20,25 @@ if (Meteor.isClient) {
 
   Template.createProfile.events({
     'submit .profile': function (event) {
-      Profiles.insert({
-        username: Meteor.user().username,
+      console.log("submitted");
+      var myProfile = {
+        _id: Meteor.userId(),
         name: event.target.name.value,
         school: event.target.school.value,
         email: event.target.email.value
-      })
+      };
+      Profiles.insert(myProfile);
     }, 
-  });
 
-  Template.createProfile.helpers({
     
   });
 
+  Template.createProfile.helpers({
+    data:function(){
+      return Profiles.findOne({_id: Meteor.userId()});
+    }
+  });
+  
 }
 
 if (Meteor.isServer) {
