c91bd402638555c62722064c5413cd73b7c314ab
Vivian Zhang <vivzhang@mit.edu>
2016-01-17 20:10:20 -0500

    added ability to edit profile

--- hackhomies.js
+++ hackhomies.js
@@ -20,14 +20,20 @@ if (Meteor.isClient) {
 
   Template.createProfile.events({
     'submit .profile': function (event) {
-      console.log("submitted");
+      //console.log("submitted");
       var myProfile = {
         _id: Meteor.userId(),
         name: event.target.name.value,
         school: event.target.school.value,
         email: event.target.email.value
       };
-      Profiles.insert(myProfile);
+      if(Profiles.findOne({_id: Meteor.userId()}))
+        Profiles.update(
+          {_id: Meteor.userId()},
+          {$set: myProfile}
+        );
+      else
+        Profiles.insert(myProfile);
     }, 
 
     
