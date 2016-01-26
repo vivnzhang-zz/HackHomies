d645bb6bb6750549bc380624866cc4a143127738
Jessie Wang <jessiebot@dhcp-18-189-11-35.dyn.MIT.EDU>
2016-01-25 19:22:21 -0500

    index changes

--- hackhomies.js
+++ hackhomies.js
@@ -7,6 +7,7 @@ if (Meteor.isClient) {
     passwordSignupFields: "USERNAME_ONLY"
   });
 
+
   Template.nav.helpers({
     teamCount: function () {
       return Profiles.findOne({_id: Meteor.userId()}).team.length;
