b021bf05851c2ae5d08f825611deb3c672e5f459
Sarah Wooders <swooders@mit.edu>
2016-01-25 21:34:36 -0500

    implemented ranking system that doesn 't work alongside search

--- hackhomies.js
+++ hackhomies.js
@@ -13,11 +13,86 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.browse.helpers({
-    profiles: function () {
-      return Profiles.find({});
-    }
-  });
+  Template.browse.rendered = function(){
+    Profiles.find({}).fetch().sort(compare);
+       function compare(p1, p2) {
+          function score(p1, p2) { 
+            var skillMatch = 0;
+            for(var i = 0; i < p1.teamSkills.length; i ++) {
+                for(var j = 0; j < p2.mySkills.length; j ++) {
+                  if(p1.teamSkills[i] == p2.mySkills[j]) {
+                    skillMatch ++;
+                    break;
+                  }
+                }
+            }
+          var interestMatch = 0;
+          for(var i = 0; i < p1.interests.length; i ++) {
+              for(var j = 0; j < p2.interests.length; j ++) {
+                if(p1.interests[i] == p2.interests[j]) {
+                  interestMatch ++;
+                  break;
+                }
+              }
+          }
+          var l1 = p1.level;
+          var l2 = p2.level;
+          return skillMatch/p1.teamSkills.length + interestMatch/p1.interests.length - Math.abs(l1 - l2);
+        }
+        var p = Profiles.findOne({_id: Meteor.userId()});
+        var score1 = score(p, p1);
+        var score2 = score(p, p2);
+        if(score1 < score2) {
+          return 1;
+        } else if(score1 > score2) {
+          return -1;
+        } else {
+          return 0;
+        }
+      }
+  };
+
+  // Template.browse.helpers({
+  //   profiles: function () {
+  //     alert('hi');
+  //     Profiles.find({}).fetch().sort(compare);
+  //      function compare(p1, p2) {
+  //         function score(p1, p2) { 
+  //           var skillMatch = 0;
+  //           for(var i = 0; i < p1.teamSkills.length; i ++) {
+  //               for(var j = 0; j < p2.mySkills.length; j ++) {
+  //                 if(p1.teamSkills[i] == p2.mySkills[j]) {
+  //                   skillMatch ++;
+  //                   break;
+  //                 }
+  //               }
+  //           }
+  //         var interestMatch = 0;
+  //         for(var i = 0; i < p1.interests.length; i ++) {
+  //             for(var j = 0; j < p2.interests.length; j ++) {
+  //               if(p1.interests[i] == p2.interests[j]) {
+  //                 interestMatch ++;
+  //                 break;
+  //               }
+  //             }
+  //         }
+  //         var l1 = p1.level;
+  //         var l2 = p2.level;
+  //         return skillMatch/p1.teamSkills.length + interestMatch/p1.interests.length - Math.abs(l1 - l2);
+  //       }
+  //       var p = Profiles.findOne({_id: Meteor.userId()});
+  //       var score1 = score(p, p1);
+  //       var score2 = score(p, p2);
+  //       if(score1 < score2) {
+  //         return 1;
+  //       } else if(score1 > score2) {
+  //         return -1;
+  //       } else {
+  //         return 0;
+  //       }
+  //     }
+  //   },
+  // });
 
   Template.teams.helpers({
     profiles: function () {
@@ -96,13 +171,13 @@ if (Meteor.isClient) {
       return size + " Team Members";
     }
   }, 
-  score: function () { //add more to this!!!
-      var p1 = Profiles.findOne({_id: Meteor.userId()});
-      var p2 = Profiles.findOne(this._id);
-      //alert("score is called");
-      if(this._id == Meteor.userId()) {
+  compatable: function() {
+    if(this._id == Meteor.userId()) {
         return false;
-      }
+    } else {
+      return score(Profiles.findOne({_id: Meteor.userId()}), Profiles.findOne(this._id)) >= 0;
+    }
+    function score(p1, p2) { //add more to this!!!
       var skillMatch = 0;
       for(var i = 0; i < p1.teamSkills.length; i ++) {
           for(var j = 0; j < p2.mySkills.length; j ++) {
@@ -123,11 +198,9 @@ if (Meteor.isClient) {
       }
       var l1 = p1.level;
       var l2 = p2.level;
-      var score  = skillMatch/p1.teamSkills.length + interestMatch/p1.interests.length - Math.abs(l1 - l2);
-      //alert(score);
-      return score >= 0;
+      return skillMatch/p1.teamSkills.length + interestMatch/p1.interests.length - Math.abs(l1 - l2);
     }
-
+  }
   });
 
   Template.createProfile.events({
