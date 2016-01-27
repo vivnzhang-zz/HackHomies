6c2edfd01b119baf0e610575ea4898a96e15d027
Vivian Zhang <vivzhang@mit.edu>
2016-01-26 22:10:29 -0500

    limit on browse removed

--- indexes.js
+++ indexes.js
@@ -3,5 +3,8 @@ Profiles = new Mongo.Collection('profiles');
 ProfilesIndex = new EasySearch.Index({
   collection: Profiles,
   fields: ['mySkills', 'name', 'school'],
-  engine: new EasySearch.MongoDB()
+  engine: new EasySearch.MongoDB(),
+  defaultSearchOptions: {
+    limit: 50
+  }
 });
