0f2c27b636d72efcbbfedd755d78b0600f8a054e
Vivian Zhang <vivzhang@mit.edu>
2016-02-02 12:49:04 -0500

    final changes

--- server/methods.js
+++ server/methods.js
@@ -1,9 +1,9 @@
 Meteor.methods({
   'updateProfile':function(myProfile) {
     console.log("server function called");
-    if(Profiles.findOne({id: Meteor.userId()})){
+    if(Profiles.findOne({_id: Meteor.userId()})){
       console.log("updating profile");
-      Profiles.update({id: Meteor.userId()}, {$set: myProfile});
+      Profiles.update({_id: Meteor.userId()}, {$set: myProfile});
     }
     else {
       console.log("adding new profile");
